ae9972e3713ec9b90b0668d22196347b493e72e5
Jonathannygaard <jonathan.nygaard1310@gmail.com>
2023-10-29 13:46:42 +0100

    Added shit

--- Data_structure/Data_structure/dllmain.cpp
+++ Data_structure/Data_structure/dllmain.cpp
@@ -401,6 +401,7 @@ public:
     std::vector<Graph_Node*> All_Nodes;
     std::vector<Edge_Node*> All_Edges;
     Graph_Node* root;
+    Edge_Node* temp_edge;
 
     /// \brief Creating a new graph node
     /// \param data Information to be stored in node (int)
@@ -455,9 +456,22 @@ public:
                   for (auto Node: Edge->Nodes)
                   {
                       Edge->Nodes.erase(Edge->Nodes.begin());
+                      temp_edge = Edge;
                   }
               }
          }
+
+       for(size_t i; i < All_Edges.size()-1; i++)
+       {
+           if(temp_edge == All_Edges[0])
+           {
+               All_Edges.erase(All_Edges.begin());
+               return;
+           }
+           All_Edges.push_back(All_Edges.front());
+           All_Edges.erase(All_Edges.begin());
+       }
+       
           for(int i = 0; i < Node->Adjacent_Nodes.size()-1; i++)
           {
                 if(Node->Adjacent_Nodes[i] == Node2)
@@ -472,6 +486,20 @@ public:
                  Node2->Adjacent_Nodes.erase(Node2->Adjacent_Nodes.begin() + i);
                 }
           }
+       if(Node->Edges.empty())
+       {
+           if(Node->Adjacent_Nodes.empty())
+           {
+               delete Node;
+           }
+       }
+       if(Node2->Edges.empty())
+       {
+           if(Node2->Adjacent_Nodes.empty())
+           {
+               delete Node2;
+           }
+       }
      }
     
      /// \brief Finding all nodes connected to a node
@@ -529,11 +557,26 @@ public:
        return Node->data;
    }
 
+    /// \brief Finding Weight of an Edge
+    /// \param Edge Edge to find weight of
     int find_weight_of_edge(Edge_Node* Edge)
    {
        return Edge->Weight;
    }
-    
+
+    /// \brief Finding a random node
+    Graph_Node* find_random_node()
+   {
+       return All_Nodes[rand() % All_Nodes.size() - 1];
+   }
+
+    /// \brief Creating a node connected to a random node from your graph
+    /// \param data Data Node should hold
+    /// \param weight Weight of edge between Nodes
+    void create_node_randomly(int data, int weight)
+   {
+       connect_nodes(find_random_node(), new_graph_node(data), weight);
+   }
 
     /// \brief Printing all nodes using depth first search
     /// \param root Node to start printing from
